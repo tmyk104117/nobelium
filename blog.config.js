5a569684f347fa2b72d4fc6b423e08d45cca7b4d
zhang shijie <tmyk104117@126.com>
2022-04-25 09:27:40 +0800

    change link

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'crypto技术',
+  author: 'simon',
+  email: 'tmyk104117@126.com',
+  link: 'https://simon-blockchain.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
